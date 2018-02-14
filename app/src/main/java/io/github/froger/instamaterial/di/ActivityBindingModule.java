824dfa20614dc1927cf995e6c708df030e7d14f4
KiyoungLee <eevee300@gmail.com>
2018-02-14 21:53:31 +0900

    Add CommentModule

--- app/src/main/java/io/github/froger/instamaterial/di/ActivityBindingModule.java
+++ app/src/main/java/io/github/froger/instamaterial/di/ActivityBindingModule.java
@@ -2,6 +2,16 @@ package io.github.froger.instamaterial.di;
 
 import dagger.Module;
 import dagger.android.ContributesAndroidInjector;
+import io.github.froger.instamaterial.ui.activity.Comment.CommentModule;
+import io.github.froger.instamaterial.ui.activity.Comment.CommentsActivity;
+import io.github.froger.instamaterial.ui.activity.Main.MainActivity;
+import io.github.froger.instamaterial.ui.activity.Main.MainModule;
+import io.github.froger.instamaterial.ui.activity.Photo.TakePhotoActivity;
+import io.github.froger.instamaterial.ui.activity.Photo.TakePhotoModule;
+import io.github.froger.instamaterial.ui.activity.Profile.UserProfileActivity;
+import io.github.froger.instamaterial.ui.activity.Profile.UserProfileModule;
+import io.github.froger.instamaterial.ui.activity.Publish.PublishActivity;
+import io.github.froger.instamaterial.ui.activity.Publish.PublishModule;
 
 /**
  * Created by KiyoungLee on 2018-02-12.
@@ -10,5 +20,24 @@ import dagger.android.ContributesAndroidInjector;
 @Module
 public abstract class ActivityBindingModule {
 
+    @ActivityScope
+    @ContributesAndroidInjector(modules = CommentModule.class)
+    abstract CommentsActivity commentsActivity();
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = MainModule.class)
+    abstract MainActivity mainActivity();
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = TakePhotoModule.class)
+    abstract TakePhotoActivity takeActivity();
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = UserProfileModule.class)
+    abstract UserProfileActivity userProfileActivity();
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = PublishModule.class)
+    abstract PublishActivity publishActivity();
 
 }
--- app/src/main/java/io/github/froger/instamaterial/ui/activity/Comment/CommentModule.java
+++ app/src/main/java/io/github/froger/instamaterial/ui/activity/Comment/CommentModule.java
@@ -1,6 +1,12 @@
 package io.github.froger.instamaterial.ui.activity.Comment;
 
+import android.view.LayoutInflater;
+
+import dagger.Binds;
 import dagger.Module;
+import dagger.Provides;
+import io.github.froger.instamaterial.di.ActivityScope;
+import io.github.froger.instamaterial.ui.adapter.CommentsAdapter;
 
 /**
  * Created by KiyoungLee on 2018-02-13.
@@ -8,4 +14,15 @@ import dagger.Module;
 
 @Module
 public abstract class CommentModule {
+
+    private final CommentsActivity activity;
+
+    public CommentModule(CommentsActivity activity) {
+        this.activity = activity;
+    }
+
+    @ActivityScope
+    @Binds
+    abstract CommentsAdapter provideCatalogAdapter(CommentsAdapter adapter);
+
 }
